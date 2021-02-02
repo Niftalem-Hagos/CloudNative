197bb1cb30203b12cb80faa430754ec222fad2f6
alem <niftalemhagos@gmail.com>
2021-02-01 21:20:48 -0600

    created CustomerService class

--- src/main/java/com/example/raai/CustomerService.java
+++ src/main/java/com/example/raai/CustomerService.java
@@ -1,5 +1,14 @@
 package com.example.raai;
 
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
 public class CustomerService {
+
+    private final DataSource dataSource = new EmbeddedDatabaseBuilder()
+            .setName("Customer").setType(EmbeddedDatabaseType.H2).build();
+
 
 }
