8db6b94c1e7ab9cae0c0d4ecd46ec46f2cf92b98
alem <niftalemhagos@gmail.com>
2021-02-01 21:43:27 -0600

    Updated CustomerService class and created Customer class

--- src/main/java/com/example/raai/CustomerService.java
+++ src/main/java/com/example/raai/CustomerService.java
@@ -1,9 +1,22 @@
 package com.example.raai;
 
+import com.example.Customer;
+import javassist.ClassPath;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class CustomerService {
 
@@ -13,7 +26,36 @@ public class CustomerService {
     public static void main(String args[]) throws Throwable {
         CustomerService customerService = new CustomerService();
 
+    DataSource dataSource = customerService.dataSource;
+        DataSourceInitializer init = new DataSourceInitializer();
+        init.setDataSource(dataSource);
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.setScripts(new ClassPathResource("schema.sql"));
+        new ClassPathResource("data.sql");
+        init.setDatabasePopulator(populator);
+        init.afterPropertiesSet();
 
+        int size = customerService.findAll().size();
+        Assert.isTrue(size == 2);
 
+    }
+
+    public Collection<Customer> findAll() {
+        List<Customer> customersList = new ArrayList<>();
+        try {
+            try(Connection c = dataSource.getConnection()) {
+                Statement statement = c.createStatement();
+                try(ResultSet rs = statement.executeQuery("select * from CUSTOMERS")) {
+                    while (rs.next()){
+                        customersList.add(new Customer(rs.getLong("ID"), rs.getString("EMAIL")));
+                    }
+
+                }
+
+            }
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return customersList;
     }
 }
